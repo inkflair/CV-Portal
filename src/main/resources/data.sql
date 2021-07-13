3103dcd5dc8a168dae7a20e2dbcddc1c626ecf62
Yan Chen <ychen3394@gatech.edu>
2021-07-12 22:24:48 -0400

    Populate template with data from postgres

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,7 @@
-INSERT INTO users (id, username, password, active, roles) values (1, 'foo', 'foo', 'true', 'USER');
+INSERT INTO users (id, username, password, active, roles) values
+(1, 'einstein', 'einstein', 'true', 'USER'),
+(2, 'newton', 'newton', 'true', 'USER');
 
-INSERT INTO user_profile (id, username, theme, summary) values
-(1, 'foo', '1', 'User name foo'),
-(2, 'bar', '1', 'User name bar');
+INSERT INTO user_profile (id, username, theme, summary, first_name, last_name, email, phone, designation) values
+(1, 'einstein', '1', 'Developed Theory of Relativity', 'Albert', 'Einstein', 'albert@einstein.com', '12345678901', 'physicist'),
+(2, 'newton', '1', 'Developed Theory of Gravity', 'Isaac', 'Newton', 'isaac@newton.com', '12345678909', 'physicist');
